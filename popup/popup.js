a4fe5acf9a91a7cc6f6bfb85b3c67b9a1a2d2f18
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2024-07-19 23:13:06 +0300

    gün farkı fonksiyonu tamamlandı

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,17 @@
-function calculateDateDifference() {
-  const startDate = new Date(document.getElementById("start-date").value);
-  const endDate = new Date(document.getElementById("end-date").value);
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const startDateValue = document.getElementById("start-date").value;
+  const endDateValue = document.getElementById("end-date").value;
+
+  if (!startDateValue || !endDateValue) {
+    document.getElementById("result").innerHTML =
+      "Lütfen her iki tarihi de doldurunuz.";
+    return;
+  }
+
+  const startDate = new Date(startDateValue);
+  const endDate = new Date(endDateValue);
   const timeDifference = endDate - startDate;
-  const dayDifference = timeDifference / (1000 * 3600 * 24);
-  document.getElementById(
-    "result"
-  ).innerHTML = `The difference is ${dayDifference} days.`;
-}
+  const dayDifference = (timeDifference / (1000 * 3600 * 24)) * -1;
+
+  document.getElementById("result").innerHTML = `${dayDifference} gün`;
+});
